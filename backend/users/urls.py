04be59e162bf584ab0b7a123f716c9fc82e361bf
Alexey Belov <alexeybwm@gmail.com>
2021-10-04 18:53:35 +0200

    djoser-urls-auth

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,14 +1,9 @@
 from django.urls import path, include
 
-from djoser import views
+# from djoser import views
 
-
-# from .views import CustomAuthToken, Logout
 
 urlpatterns = [
     path('', include('djoser.urls')),
-    path('auth/token/login/',
-         views.TokenCreateView.as_view(), name='login'),
-    path('auth/token/logout/',
-         views.TokenDestroyView.as_view(), name='logout'),
+    path('auth/', include('djoser.urls.authoken')),
 ]
