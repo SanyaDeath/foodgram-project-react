d648aa8809535032251f0d5fd872f67772d54cb3
Alexey Belov <alexeybwm@gmail.com>
2021-10-04 19:01:03 +0200

    djoser-urls-try2

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path, include
 
-# from djoser import views
-
 
 urlpatterns = [
-    path('', include('djoser.urls')),
+    #path('', include('djoser.urls')),
     path('auth/', include('djoser.urls.authoken')),
 ]
