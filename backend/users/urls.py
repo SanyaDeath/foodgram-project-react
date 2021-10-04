e3a2630a2d6d19d94fa29865853c21630c188457
Alexey Belov <alexeybwm@gmail.com>
2021-10-04 14:15:47 +0200

    flake

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 
-djoser import views
+from djoser import views
 
 
-from .views import CustomAuthToken, Logout
+# from .views import CustomAuthToken, Logout
 
 urlpatterns = [
     path('', include('djoser.urls')),
     path('auth/token/login/', views.TokenCreateView.as_view(), name='login'),
-    path('auth/token/logout/', views.TokenDestroyView.as_view(), name='logout'),
+    path('auth/token/logout/',
+         views.TokenDestroyView.as_view(), name='logout'),
 ]
--- backend/users/views.py
+++ backend/users/views.py
@@ -1,11 +1,11 @@
 from django.contrib.auth import get_user_model
-from rest_framework import status
-from rest_framework.authtoken.models import Token
-from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.response import Response
-from rest_framework.views import APIView
+# from rest_framework import status
+# from rest_framework.authtoken.models import Token
+# from rest_framework.authtoken.views import ObtainAuthToken
+# from rest_framework.response import Response
+# from rest_framework.views import APIView
 
-from .serializers import AuthTokenSerializer
+# from .serializers import AuthTokenSerializer
 
 User = get_user_model()
 
