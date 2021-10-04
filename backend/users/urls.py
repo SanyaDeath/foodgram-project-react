c1b6f38b9e9f9b83fff2be4cfd433c998fa3ea51
Alexey Belov <alexeybwm@gmail.com>
2021-10-04 19:03:24 +0200

    dry-fix-2

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    #path('', include('djoser.urls')),
+    # path('', include('djoser.urls')),
     path('auth/', include('djoser.urls.authoken')),
 ]
