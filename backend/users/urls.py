0e44d579840ebd1250629b9f49e2ebb4bcc84ea9
Alexey Belov <alexeybwm@gmail.com>
2021-10-04 23:19:40 +0200

    routers

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path, include
 
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+
 
 urlpatterns = [
-    # path('', include('djoser.urls')),
+    path('', include(router.urls)),
     path('auth/', include('djoser.urls.authtoken')),
 ]
